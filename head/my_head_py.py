371d11db0acd9c3a76a8b6efd58551ad397f40be
Haostar <460954908@qq.com>
2026-04-16 13:49:54 +0800

    py

---
+++ head/my_head_py.py
@@ -0,0 +1,27 @@
+import sys
+import math
+import heapq
+import bisect
+import itertools
+import functools
+from collections import deque, Counter, defaultdict, OrderedDict
+input = sys.stdin.readline
+
+# a, b, c = map(int, input().split()) 读同一行
+
+def init():
+    pass
+
+def solve():
+    pass
+
+def main():
+    init()
+    _ = 1 
+    # _ = int(input())
+    while _ > 0:
+        solve()
+        _ -= 1
+
+if __name__ == "__main__":
+    main()
